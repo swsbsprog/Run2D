8acd832462c3019b7e8e8c12aac699c8b6e2841a
hktrollgames <hktrollgames@gmail.com>
2021-06-23 16:20:53 +0900

    테스트 편하게 하기 진행중

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Animator animator;
-
+    Rigidbody2D rigid;
+    public Vector2 jumpForce = new Vector2(0, 1000);
+    public float gravityScale = 7;
     void Start()
     {
         //animator = transform.Find("Sprite").GetComponent<Animator>();
         animator = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.gravityScale = gravityScale;
         animator.Play("Run");
     }
     public float speed = 20;
     void Update()
     {
         transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(jumpForce);
+        }
     }
 }
