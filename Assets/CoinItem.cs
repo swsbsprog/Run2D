1a5cb996501721e06dddb42a471faed8930a0562
hktrollgames <hktrollgames@gmail.com>
2021-06-25 07:45:04 +0900

    코인 추가

---
+++ Assets/CoinItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinItem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("Coin Enter:" + collision.transform);
+        GetComponentInChildren<Animator>().Play("HideCoin", 1);
+    }
+}
