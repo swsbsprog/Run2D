246b90928a0a720ed215ae57bd095fa68a861ff4
hktrollgames <hktrollgames@gmail.com>
2021-06-25 20:17:01 +0900

    자석효과 가속도 적용

--- Assets/MagnetAbility.cs
+++ Assets/MagnetAbility.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MagnetAbility : MonoBehaviour
@@ -10,20 +11,33 @@ public class MagnetAbility : MonoBehaviour
         if (collision.transform.GetComponent<CoinItem>() == null)
             return;
 
+        if (items.ContainsKey(collision.transform))
+            return;
+
         items[collision.transform] = 0;
     }
 
-    public float accelerate = 30;
+    public float accelerate = 30; // 초당 가속도.
+
+    Dictionary<Transform, float> tmpItems = new Dictionary<Transform, float>();
     private void Update()
     {
         var pos = transform.position;
-        
-        foreach(var item in items)
+
+        tmpItems.Clear();
+        foreach (var item in items)
+        {
+            tmpItems[item.Key] = item.Value;
+        }
+
+        foreach (var item in tmpItems)
         {
             var coinTr = item.Key;
-            float acceleration = item.Value;
+            float acceleration = item.Value + accelerate * Time.deltaTime;
+            items[item.Key] = acceleration;
+
             Vector2 dir = (pos - coinTr.position).normalized;
-            Vector2 move = dir * (acceleration + accelerate) * Time.deltaTime;
+            Vector2 move = dir * (acceleration) * Time.deltaTime;
             coinTr.Translate(move);
         }
     }
