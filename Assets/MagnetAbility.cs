87f75749a06ed0e198d91944bede4ecc06062df4
hktrollgames <hktrollgames@gmail.com>
2021-06-25 20:27:15 +0900

    가속도 버그 수정

--- Assets/MagnetAbility.cs
+++ Assets/MagnetAbility.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 
 public class MagnetAbility : MonoBehaviour
 {
-    Dictionary<Transform, float> items = new Dictionary<Transform, float>(); //<자석에 이끌린 TR, 가속도>
+    class RefFloat
+    {
+        public float acc;
+    }
+    Dictionary<Transform, RefFloat> items = new Dictionary<Transform, RefFloat>(); //<자석에 이끌린 TR, 가속도>
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.GetComponent<CoinItem>() == null)
@@ -14,27 +18,20 @@ public class MagnetAbility : MonoBehaviour
         if (items.ContainsKey(collision.transform))
             return;
 
-        items[collision.transform] = 0;
+        items[collision.transform] = new RefFloat();
     }
 
     public float accelerate = 30; // 초당 가속도.
 
-    Dictionary<Transform, float> tmpItems = new Dictionary<Transform, float>();
     private void Update()
     {
         var pos = transform.position;
 
-        tmpItems.Clear();
         foreach (var item in items)
         {
-            tmpItems[item.Key] = item.Value;
-        }
-
-        foreach (var item in tmpItems)
-        {
             var coinTr = item.Key;
-            float acceleration = item.Value + accelerate * Time.deltaTime;
-            items[item.Key] = acceleration;
+            float acceleration = item.Value.acc + accelerate * Time.deltaTime;
+            items[item.Key].acc = acceleration;
 
             Vector2 dir = (pos - coinTr.position).normalized;
             Vector2 move = dir * (acceleration) * Time.deltaTime;
