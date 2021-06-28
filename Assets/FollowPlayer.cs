0d58a82676e8d2d2e7b9d794181d9a37c5edb7f2
hktrollgames <hktrollgames@gmail.com>
2021-06-28 19:10:09 +0900

    카메라 플레이어 위치로 이동

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public float lerp = 0.01f;
+
+    void Update()
+    {
+        var newPos = transform.position;
+        newPos.x = Mathf.Lerp(newPos.x,  Player.instance.transform.position.x, lerp);
+        transform.position = newPos;
+    }
+}
