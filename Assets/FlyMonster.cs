87146d7d5151fc640179069116904aca8292cb65
hktrollgames <hktrollgames@gmail.com>
2021-06-29 14:18:30 +0900

    날아다니는 적 움직임 구현

--- Assets/FlyMonster.cs
+++ Assets/FlyMonster.cs
@@ -1,18 +1,25 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Pathfinding;
 public class FlyMonster : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AIPath aiPath;
     void Start()
     {
-        
+        aiPath = GetComponent<AIPath>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 }
