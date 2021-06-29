4c6e9a338c1711b0065db1e016f2d4cd435aa3bf
hktrollgames <hktrollgames@gmail.com>
2021-06-29 10:48:39 +0900

    몬스터 좌우 이동

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -5,6 +5,59 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    private void Start()
+    {
+        InitWorldMoveArea();
+        StartCoroutine(PatrolCo());
+    }
+
+    private void InitWorldMoveArea()
+    {
+        minWorldMoveX = transform.position.x + minLocalMoveX;
+        maxWorldMoveX = transform.position.x + maxLocalMoveX;
+    }
+
+    public float speed = 3;
+    public float minLocalMoveX = -5;
+    public float maxLocalMoveX = 5;
+    public float minWorldMoveX;
+    public float maxWorldMoveX;
+    public DirectionType direction = DirectionType.Right;
+    public enum DirectionType
+    {
+        Right,
+        Left,
+    }
+    private IEnumerator PatrolCo()
+    {
+        while (true)
+        {
+            while (true)
+            {
+                if(direction == DirectionType.Right)
+                {
+                    if (maxWorldMoveX < transform.position.x)
+                    {
+                        direction = DirectionType.Left;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (minWorldMoveX > transform.position.x)
+                    {
+                        direction = DirectionType.Right;
+                        break;
+                    }
+                }
+
+                float move = direction == DirectionType.Right ? speed : -speed;
+                transform.Translate(move * Time.deltaTime, 0, 0);
+                yield return null;
+            }
+        }
+    }
+
     public int hp = 10;
     internal void OnDamge(int damage)
     {
